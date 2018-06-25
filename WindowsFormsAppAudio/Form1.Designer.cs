35abd1ec51ca3184775f4ee07b29ec6793f57590
Max Yorro <maxyorro@gmail.com>
2018-06-25 15:24:44 -0400

    Add project files.

---
+++ WindowsFormsAppAudio/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsAppAudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AudioLabel = new System.Windows.Forms.Label();
+            this.AudioFileLocationTextBox = new System.Windows.Forms.TextBox();
+            this.Browse1Button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TimestampNameRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.BrowseJPGButton = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.ComputeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AudioLabel
+            // 
+            this.AudioLabel.AutoSize = true;
+            this.AudioLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AudioLabel.Location = new System.Drawing.Point(12, 26);
+            this.AudioLabel.Name = "AudioLabel";
+            this.AudioLabel.Size = new System.Drawing.Size(144, 20);
+            this.AudioLabel.TabIndex = 0;
+            this.AudioLabel.Text = "Audio File Location";
+            // 
+            // AudioFileLocationTextBox
+            // 
+            this.AudioFileLocationTextBox.Location = new System.Drawing.Point(183, 26);
+            this.AudioFileLocationTextBox.Name = "AudioFileLocationTextBox";
+            this.AudioFileLocationTextBox.Size = new System.Drawing.Size(433, 20);
+            this.AudioFileLocationTextBox.TabIndex = 1;
+            // 
+            // Browse1Button
+            // 
+            this.Browse1Button.Location = new System.Drawing.Point(652, 17);
+            this.Browse1Button.Name = "Browse1Button";
+            this.Browse1Button.Size = new System.Drawing.Size(90, 40);
+            this.Browse1Button.TabIndex = 2;
+            this.Browse1Button.Text = "Browse";
+            this.Browse1Button.UseVisualStyleBackColor = true;
+            this.Browse1Button.Click += new System.EventHandler(this.Browse1Button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Timestamps and names";
+            // 
+            // TimestampNameRichTextBox
+            // 
+            this.TimestampNameRichTextBox.Location = new System.Drawing.Point(197, 72);
+            this.TimestampNameRichTextBox.Name = "TimestampNameRichTextBox";
+            this.TimestampNameRichTextBox.Size = new System.Drawing.Size(545, 239);
+            this.TimestampNameRichTextBox.TabIndex = 4;
+            this.TimestampNameRichTextBox.Text = "";
+            // 
+            // BrowseJPGButton
+            // 
+            this.BrowseJPGButton.Location = new System.Drawing.Point(764, 72);
+            this.BrowseJPGButton.Name = "BrowseJPGButton";
+            this.BrowseJPGButton.Size = new System.Drawing.Size(140, 115);
+            this.BrowseJPGButton.TabIndex = 5;
+            this.BrowseJPGButton.Text = "Compute Timestamps";
+            this.BrowseJPGButton.UseVisualStyleBackColor = true;
+            this.BrowseJPGButton.Click += new System.EventHandler(this.BrowseJPGButton_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(199, 345);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(90, 186);
+            this.listBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(309, 345);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(433, 186);
+            this.listBox2.TabIndex = 7;
+            // 
+            // ComputeButton
+            // 
+            this.ComputeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComputeButton.Location = new System.Drawing.Point(408, 583);
+            this.ComputeButton.Name = "ComputeButton";
+            this.ComputeButton.Size = new System.Drawing.Size(419, 108);
+            this.ComputeButton.TabIndex = 8;
+            this.ComputeButton.Text = "Compute";
+            this.ComputeButton.UseVisualStyleBackColor = true;
+            this.ComputeButton.Click += new System.EventHandler(this.ComputeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 752);
+            this.Controls.Add(this.ComputeButton);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BrowseJPGButton);
+            this.Controls.Add(this.TimestampNameRichTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Browse1Button);
+            this.Controls.Add(this.AudioFileLocationTextBox);
+            this.Controls.Add(this.AudioLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AudioLabel;
+        private System.Windows.Forms.TextBox AudioFileLocationTextBox;
+        private System.Windows.Forms.Button Browse1Button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox TimestampNameRichTextBox;
+        private System.Windows.Forms.Button BrowseJPGButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button ComputeButton;
+    }
+}
+
---
+++ WindowsFormsAppAudio/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace WindowsFormsAppAudio
+{
+    public partial class Form1 : Form
+    {
+        List<string> Timestamps = new List<string>();
+        List<string> Names = new List<string>();
+        string musicFileLocation = "";
+        string fileFolder = "";
+        string[] stringSeparators = new string[] { ".mp3" };
+        string[] result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse1Button_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = System.IO.File.ReadAllText(file);
+                    AudioFileLocationTextBox.Text = file;
+                    musicFileLocation = file;
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine(size);  //Shows file size for debugging
+
+            Console.WriteLine(result); //
+            Timestamps.Clear();
+            Names.Clear();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void BrowseJPGButton_Click(object sender, EventArgs e)
+        {
+            using (StringReader reader = new StringReader(TimestampNameRichTextBox.Text))
+            {
+                string line;
+                string Name = "";
+                int value;
+                while((line = reader.ReadLine()) != null)
+                {
+                    //Do something with the line
+                    bool colon = false;
+                    String[] elements = line.Split();
+                    foreach(string element in elements)
+                    {
+                        //element added to Timestamps
+                        if (element.Contains(':') && !colon && int.TryParse(element.Split(':')[0], out value) && int.TryParse(element.Split(':')[1], out value)) 
+                        {
+                            Timestamps.Add(element);
+                            colon = true;  //Set colon to true.  We are only interested in the beginning time, not the end times.
+                        }
+                        //element will be added to Names
+                        else
+                        {
+                            if (!element.Contains('\\') && !element.Contains('/'))
+                            {
+                                var charsToRemove = new string[] { "?", "@", ";", "!", "#", ":", "\""};
+
+
+
+                                string modified = element;
+                                foreach(var c in charsToRemove)
+                                {
+                                    modified = modified.Replace(c, string.Empty);
+                                    string NoNumbers = new String(modified.Where(cd => (cd < '0' || cd > '9')).ToArray());
+                                    modified = NoNumbers;
+                                }
+                                Name += " " + modified;
+                            }
+                        }
+                    }
+
+                    //Check if the string is not just whitespaces.  
+                    if(!String.IsNullOrWhiteSpace(Name))
+                        Names.Add(Name);
+                    Name = "";
+                }
+            }
+
+
+            //Put the information into the list boxes.
+            listBox1.DataSource = Timestamps;
+            listBox2.DataSource = Names;
+
+        }
+
+        private void ComputeButton_Click(object sender, EventArgs e)
+        {
+            if(musicFileLocation == "")
+            {
+                Browse1Button_Click(sender, e);
+                ComputeButton_Click(sender, e);
+            }
+            else
+            {
+                //What if the file has a period within it?
+                //fileFolder = musicFileLocation.Split('.')[0];  //Split by the .extension and give me the first part
+                fileFolder = musicFileLocation.Split(stringSeparators, StringSplitOptions.None)[0];
+                System.IO.Directory.CreateDirectory(fileFolder);
+
+                var Timestamp = Timestamps.GetEnumerator();
+                var NextTimestamp = Timestamps.GetEnumerator();
+                var Name = Names.GetEnumerator();
+                Name.MoveNext();
+                NextTimestamp.MoveNext();
+                NextTimestamp.MoveNext();
+
+
+
+                int count = 1;
+                while (Timestamp.MoveNext())
+                {
+                    //If this is the last timestamp/song, then the nextTimestamp.Current will be null.
+                    if (NextTimestamp.Current == null)
+                    {
+                        TrimMp3(musicFileLocation, System.IO.Path.Combine(fileFolder, Name.Current + ".mp3"), computeSecs(Timestamp.Current), new Mp3FileReader(musicFileLocation).TotalTime);
+                    }
+                    else
+                    {
+                        TrimMp3(musicFileLocation, System.IO.Path.Combine(fileFolder, Name.Current + ".mp3"), computeSecs(Timestamp.Current), computeSecs(NextTimestamp.Current));
+                    }
+
+                    //Adjusting the file tag information
+                    var file = TagLib.File.Create(System.IO.Path.Combine(fileFolder, Name.Current +".mp3"));
+                    file.Tag.Track = (uint)count;
+                    file.Tag.TrackCount = (uint)Timestamps.Count();
+                    file.Save();
+
+                    Name.MoveNext();
+                    NextTimestamp.MoveNext();
+                    count++;
+                }
+
+            }
+        }
+
+
+        public TimeSpan computeSecs(string TimeStamp)
+        {
+            int hours = 0;
+            int minutes = 0;
+            int seconds = 0;
+
+            //Return the end if null
+            if (TimeStamp == null)
+                return new Mp3FileReader(musicFileLocation).TotalTime;
+
+
+            string[] Parts = TimeStamp.Split(':');
+            Console.WriteLine("The timestamp had " + Parts.Length + " number of elements");
+
+            if(Parts.Length == 3)
+            {
+                Int32.TryParse(Parts[0], out hours);
+                
+                Int32.TryParse(Parts[1], out minutes);
+
+                Int32.TryParse(Parts[2], out seconds);
+
+                return new TimeSpan(hours, minutes, seconds);//hours + minutes + seconds;
+            }
+            else
+            {
+                Int32.TryParse(Parts[0], out minutes);
+
+                Int32.TryParse(Parts[1], out seconds);
+
+                return new TimeSpan(hours, minutes, seconds);
+            }
+        }
+
+
+        public static void TrimWavFile(string inPath, string outPath, TimeSpan cutFromStart, TimeSpan cutFromEnd)
+        {
+            using (WaveFileReader reader = new WaveFileReader(inPath))
+            {
+                using (WaveFileWriter writer = new WaveFileWriter(outPath, reader.WaveFormat))
+                {
+                    int bytesPerMillisecond = reader.WaveFormat.AverageBytesPerSecond / 1000;
+
+                    int startPos = (int)cutFromStart.TotalMilliseconds * bytesPerMillisecond;
+                    startPos = startPos - startPos % reader.WaveFormat.BlockAlign;
+
+                    int endBytes = (int)cutFromEnd.TotalMilliseconds * bytesPerMillisecond;
+                    endBytes = endBytes - endBytes % reader.WaveFormat.BlockAlign;
+                    int endPos = (int)reader.Length - endBytes;
+
+                    TrimWavFile(reader, writer, startPos, endPos);
+                }
+            }
+        }
+
+        private static void TrimWavFile(WaveFileReader reader, WaveFileWriter writer, int startPos, int endPos)
+        {
+            reader.Position = startPos;
+            byte[] buffer = new byte[1024];
+            while (reader.Position < endPos)
+            {
+                int bytesRequired = (int)(endPos - reader.Position);
+                if (bytesRequired > 0)
+                {
+                    int bytesToRead = Math.Min(bytesRequired, buffer.Length);
+                    int bytesRead = reader.Read(buffer, 0, bytesToRead);
+                    if (bytesRead > 0)
+                    {
+                        writer.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+
+
+        void TrimMp3(string inputPath, string outputPath, TimeSpan? begin, TimeSpan? end)
+        {
+            if (begin.HasValue && end.HasValue && begin > end)
+                throw new ArgumentOutOfRangeException("end", "end should be greater than begin");
+
+            using (var reader = new Mp3FileReader(inputPath))
+            using (var writer = System.IO.File.Create(outputPath))
+            {
+                Mp3Frame frame;
+                while ((frame = reader.ReadNextFrame()) != null)
+                    if (reader.CurrentTime >= begin || !begin.HasValue)
+                    {
+                        if (reader.CurrentTime <= end || !end.HasValue)
+                            writer.Write(frame.RawData, 0, frame.RawData.Length);
+                        else break;
+                    }
+            }
+        }
+    }
+
+}
